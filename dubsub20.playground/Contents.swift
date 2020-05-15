1d699491d80b962ce28281a6e62f761972931ba5
rif2d <saya@rifqi.id>
2020-05-16 03:22:49 +0700

    Remove GameViewController

--- dubsub20.playground/Contents.swift
+++ dubsub20.playground/Contents.swift
@@ -1,11 +1,16 @@
 import PlaygroundSupport
-import UIKit
+import SpriteKit
 
-let gameVC = GameViewController()
-
-gameVC.preferredContentSize = CGSize(
+let sceneView = SKView(frame: CGRect(
+    x:0,
+    y:0,
     width: 640,
     height: 480
-)
+))
 
-PlaygroundPage.current.liveView = gameVC
+if let scene = GameScene(fileNamed: "GameScene") {
+    scene.scaleMode = .aspectFill
+    sceneView.presentScene(scene)
+}
+
+PlaygroundPage.current.liveView = sceneView
